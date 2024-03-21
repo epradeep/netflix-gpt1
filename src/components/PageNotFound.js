b1a9a004717c338adb772074a945349be25a4276
Pradeep Etika <etikapradeep@gmail.com>
2024-03-21 13:17:09 +0530

    added page not found route

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-60">
+      <h1 className="text-2xl text-rose-600">Page not found!!</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
